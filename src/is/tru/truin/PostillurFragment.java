405fdb73c121f2f6d77bc634bedc4694081b6dba
snh11 <snh11@hi.is>
2014-04-10 23:25:25 +0000

    Postillur

--- src/is/tru/truin/PostillurFragment.java
+++ src/is/tru/truin/PostillurFragment.java
@@ -1,11 +1,23 @@
 package is.tru.truin;
 
-import is.tru.truin.R;
+import org.json.JSONObject;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
 
+import util.JSONParser;
+import util.XMLParser;
+import is.tru.truin.R;
+import is.tru.truin.MainActivity.LoadPostillur;
+
+import android.annotation.SuppressLint;
 import android.app.ListFragment;
+import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
@@ -16,42 +28,49 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 public class PostillurFragment extends ListFragment {
-		
 	
+	static final String URL = "http://api.androidhive.info/pizza/?format=xml";
+
+	static final String KEY_ITEM = "item";
+	static final String KEY_ID = "id";
+	static final String KEY_NAME = "name";
+	static final String KEY_COST = "cost";
+	static final String KEY_DESC = "description";
+	
+	TextView id;
+	TextView name;
+	TextView cost;
+	TextView desc;
+	
+	String str_id;
+	String str_name;
+	String str_cost;
+	String str_desc;
+	
+	View rootView;
+		
 	public PostillurFragment(){}
 	
-	String[] string_id = Constants.id;
-	String[] string_name = Constants.name;
-	String[] string_cost = Constants.cost;
-	String[] string_desc = Constants.desc;
 	
 	public class MyListAdapter extends ArrayAdapter<String> {
 		  
 		Context myContext;
 	
 		public MyListAdapter(Context context, int textViewResourceId,
-			String[] objects, String[] string_name, String[] string_cost, String[] string_desc) {
-			super(context, textViewResourceId, objects);
+			String str_id, String str_name, String str_cost, String str_desc) {
+			super(context, textViewResourceId);
 			myContext = context;
 		}
 
 		@Override
 		public View getView(int position, View convertView, ViewGroup parent) {
+			
+			new LoadPostillur().execute();
 			   
 			LayoutInflater inflater = 
 					(LayoutInflater)myContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
 			View row=inflater.inflate(R.layout.postillur_row, parent, false);
-			TextView id=(TextView)row.findViewById(R.id.id);
-			TextView name=(TextView)row.findViewById(R.id.name);
-			TextView cost=(TextView)row.findViewById(R.id.cost);
-			TextView desc=(TextView)row.findViewById(R.id.desc);
-			
-			id.setText(string_id[position]);
-			name.setText(string_name[position]);
-			cost.setText(string_cost[position]);
-			desc.setText(string_desc[position]);
-			
-						
+							
 			return row;
 		}
 	}
@@ -61,7 +80,7 @@ public class PostillurFragment extends ListFragment {
 		super.onCreate(savedInstanceState);
 		
 		MyListAdapter myListAdapter = 
-				new MyListAdapter(getActivity(), R.layout.postillur_row, string_id, string_name, string_cost, string_desc);
+				new MyListAdapter(getActivity(), R.layout.postillur_row, str_id, str_name, str_cost, str_desc);
 		setListAdapter(myListAdapter);
 	}
 
@@ -81,6 +100,62 @@ public class PostillurFragment extends ListFragment {
 				getListView().getItemAtPosition(position).toString(), 
 				Toast.LENGTH_LONG).show();
 	}
+	
+    private class LoadPostillur extends  AsyncTask<Void, Void, Void>{
+    	
+    	ProgressDialog pDialog;
+    	
+    	protected Void doInBackground(Void...params) {
+	    	try {
+	    		
+				XMLParser parser = new XMLParser();
+				String xml = parser.getXmlFromUrl(URL);
+				Document doc = parser.getDomElement(xml);
+
+				NodeList nl = doc.getElementsByTagName(KEY_ITEM);
+	
+				for (int i = 0; i < nl.getLength(); i++) {
+
+					Element e = (Element) nl.item(i);
+					
+					str_id = parser.getValue(e, KEY_ID);
+					str_name = parser.getValue(e, KEY_NAME);
+					str_cost = parser.getValue(e, KEY_COST);
+					str_desc = parser.getValue(e, KEY_DESC);
+										
+				}
+				return null;
+				
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+	    	return null;
+    	}
+		@Override
+		protected void onPreExecute(){
+			super.onPreExecute();
+			
+			id = (TextView)rootView.findViewById(R.id.id);
+			name = (TextView)rootView.findViewById(R.id.name);
+			cost = (TextView)rootView.findViewById(R.id.cost);
+			desc = (TextView)rootView.findViewById(R.id.desc);
+			CharSequence bidid = "Vinsamlega bíðið";
+			CharSequence sendi = "Sæki postillur"; 
+			pDialog = ProgressDialog.show(getActivity(), bidid, sendi, true, false);
+		}
+		
+		@Override
+		protected void onPostExecute(Void aVoid){
+			super.onPostExecute(aVoid);
+			id.setText(str_id);
+			name.setText(str_name);
+			cost.setText(str_cost);
+			desc.setText(str_desc);
+			pDialog.dismiss();
+		}
+
+    }
+	
 	
 
 }
