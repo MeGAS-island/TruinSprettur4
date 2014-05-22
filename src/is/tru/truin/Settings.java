34595ed92420511e541cd660f2ea438137fcf164
Gudny Lara Gudmundsdottir <gudnylara7@gmail.com>
2014-05-22 14:29:27 +0000

    Vinna í notification og laga overlap

--- src/is/tru/truin/Settings.java
+++ src/is/tru/truin/Settings.java
@@ -1,6 +1,7 @@
 package is.tru.truin;
 
 import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
@@ -30,14 +31,27 @@ public class Settings extends Activity {
 			public void onClick(View v) {
 				int selectedId = radioGroup.getCheckedRadioButtonId();
 				if(selectedId==2131361794){
+					Log.d("sound","breyting á sound");
 					Constants.isSound=!Constants.isSound;
+					Constants.isNotification = true;
 				}
 				else if(selectedId==2131361795){
+					Log.d("vib", "breyting á vibrate");
 					Constants.isVibrate = !Constants.isVibrate;
+					Constants.isNotification = true;
 				}
 				else if(selectedId==2131361796){
 					Constants.isNotification = !Constants.isNotification;
+					
+					if(Constants.isNotification==true){
+						Log.d("not", "kveikt á notification");
+						Constants.isSound=true;
+						Constants.isVibrate=true;
+						Context context = getApplicationContext();
+						NotificationReceiver.setAlarm(context, Constants.hour, Constants.min);
+					}
 				}
+				//bæta inn til baka á upphafsskjá
 			}	
 		});
 	}
